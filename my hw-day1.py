0c44d2933ee9443727e81799eda1cce773abf007
dovile <dovime@gmail.com>
2022-09-06 21:29:49 +0300

    day1

---
+++ my hw-day1.py
@@ -0,0 +1,15 @@
+# * print your name 5 times
+print("Dovile "*5)
+# * print Bananananana using "Ba" once and "na" 5 times
+print("Ba"+"na"*5)
+# * print the number of seconds in a year calculated from months, days, minutes, seconds
+months = 12
+days = ((365)/12)
+hours = 24
+minutes = 60
+seconds = 60*minutes*hours*days*months
+print(seconds)
+# * how much a googol is that is 10 to the power of the 100th
+print(10**100)
+# * how much will your 1000 Euro investment be worth after 12 years of 6% yearly interest?
+print(1000*(1+0.06)**12)
