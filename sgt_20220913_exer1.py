4c88102a7c2f529b74ea111a1f158c08fa6cb120
dovile <dovime@gmail.com>
2022-09-13 20:48:42 +0300

    ex1

---
+++ sgt_20220913_exer1.py
@@ -0,0 +1,21 @@
+# # 1. FizzBuzz 
+
+# Print a string 1,2,3,4, Fizz, 6, Buzz, 8, ..... 34, FizzBuzz, 36, .... to 97, Buzz, 99, Fizz 
+
+# So if number divided by 5 then Fizz If divided by 7 then Buzz,
+#  If divided by 5 AND 7 then FizzBuzz otherwise the same number
+
+#  Note: such a task became popular as the first task to ask to determine 
+#  whether a person knows about programming at all smile
+
+i = 1
+for i in range(1,100):
+    #print(i)
+    if  i % 5 == 0 and i % 7 == 0:
+        print("FizzBuzz")
+    elif i % 5 == 0:
+        print("Fizz")
+    elif i % 7 == 0:
+        print("Buzz")
+    else:
+        print(i)
---
+++ test2.py
@@ -0,0 +1,8 @@
+my_temperature = int(input('What is your temperetature?'))
+if my_temperature < 35:
+    print("not too cold")
+if 35 <= my_temperature < 37:
+    print("all right")
+if my_temperature >= 37:
+    print("possible fever")
+
