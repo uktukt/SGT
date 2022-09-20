485221c385b3c817962e54159d54289975fc7e94
dovile <dovime@gmail.com>
2022-09-20 21:13:23 +0300

    updated

---
+++ sgt_20220920_exer3.py
@@ -0,0 +1,21 @@
+# 3. Reversed words
+
+# The user enters a sentence.
+
+# We output all the words of the sentence in a reverse form. 
+# - not the whole text reversed!!
+
+# Example
+
+# Alus kauss mans -> Sula ssuak snam
+# notice how each word was reversed separately
+
+# PS Split and join operations could be useful here.
+
+# text = input("enter sentence ")
+text = as noriu valgyti
+
+text_list = []
+
+for words in text:
+    print(words)
