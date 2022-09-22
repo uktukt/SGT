b9578dc34e5936c5aa7d79f59e947c8e3f94d835
dovile <dovime@gmail.com>
2022-09-22 20:41:20 +0300

    ex1

---
+++ sgt_20220922_fun_exer1.py
@@ -0,0 +1,22 @@
+#1. The Big Result
+# Write an add_mult function that requires three parameters / arguments
+# Returns the result that is the sum of the 2 smallest arguments multiplied by the largest argument value.
+# PS Assume that numeric parameters will always be passed to the function, no need to check types
+# Various solutions are possible (you are allowed to use other data structures inside function such as list).
+# Example add_mult (2,5,4) -> will return (2 + 4) * 5 = 30
+# PSS function should return the result, not print it.
+
+def add_mult(a, b , c):
+    if a < b and b < c or a==b or a==b==c:
+        result = (a + b) * c
+        return result
+    if a < b and c < b or a==c:
+        result = (a + c) * b
+        return result
+    if a > b and a > c or b==c:
+        result = (b + c) * a
+        return result
+
+result2 = add_mult(1, 1, 1)
+print(result2)
+
