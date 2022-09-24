4ed8d411304ca0c53b92f22d17f80b7598e2ab8d
dovile <dovime@gmail.com>
2022-09-24 20:44:50 +0300

    updated

--- sgt_20220920_exer1.py
+++ sgt_20220920_exer1.py
@@ -9,4 +9,24 @@
 
 # PS Exit the program  by entering "q"
 
-# 1c The program does not show all the numbers entered but only TOP3 and BOTTOM3 and of course still average.
+# 1c The pro
+
+
+# while True:
+#     user_input = input(
+#         "Please enter numbers(separated by comma) or 'q' to quit: ")
+#     if user_input == "q":
+#         print("You quit")
+#         break
+
+#     else:
+#         list_numbers.append(float(user_input))
+#         sorted_number_list = sorted(list_numbers) 
+#         # if we did not need original order
+#         # then we could have used list_numbers.sort() instead
+#         average_calculated = sum(list_numbers) / len(list_numbers)
+#         print(
+#             f"Numbers you entered are: {list_numbers} , average is: {average_calculated}")
+
+#         print(f"Top 3 values you enetered are: {sorted_number_list[-3:]}")
+#         print(f"Bottom 3 values you entered are: {sorted_number_list[:3]}")
