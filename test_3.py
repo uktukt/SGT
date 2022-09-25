bd65da4f4debe66e88a44b254b42e867ee6d775a
dovile <dovime@gmail.com>
2022-09-25 11:51:37 +0300

    updated

--- test_3.py
+++ test_3.py
@@ -1,8 +1,8 @@
 p0 = 1000
-perc = -3
-delta = 50
-target_p = 2000
-years = 20
+perc = 2
+delta = -100
+target_p = 5000
+years = 125
 
 sum1 = p0 * (1 + perc*0.01)**years + delta * (1 + perc*0.01)**(years-1)
 sum2 = delta * (years -1)
