612f427cd223f9e091b3b7add4c40683cc22070e
dovile <dovime@gmail.com>
2022-10-01 19:47:29 +0300

    updated

---
+++ sgt_20220927_tuples_exer3.py
@@ -0,0 +1,13 @@
+# 3. Is there a pangram?
+
+# Write a function is_pangram(text, alphabet='abcdefghijklmnopqrstuvwxyz')
+
+# that returns True when the text parameter contains all the letters passed in an alphabet.
+# We return False otherwise
+
+
+# pangram - sentence, word string containing all letters of the alphabet - https://en.wikipedia.org/wiki/Pangram
+
+# We ignore spaces and believe that uppercase is as valid as lowercase, i. here A and a -> a
+
+
