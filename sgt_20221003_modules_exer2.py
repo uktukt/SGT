b465b9c707b9cc1230fcd60287d7ce99b3c82e81
dovile <dovime@gmail.com>
2022-10-06 20:40:53 +0300

    updated

---
+++ sgt_20221003_modules_exer2.py
@@ -0,0 +1,19 @@
+# 2. Deck - probably for homework, see how far you get
+
+# write a class Deck with the following attributes(variables)
+# available - contains list of card tuples that can be used
+# spent - contains list of card tuples that have been used
+# there should be following methods:
+
+# constructor (__init__) method
+# initializes available with full 52 card list of tuples
+# initializes spent with empty list
+# shuffle(self):
+# # shuffles available cards - works just like 1st function but works on available
+
+# get_cards(self, count=1):
+# # gets some number(default 1) of cards from available 
+# adds these cards to spent
+# returns these cards
+
+# # you can add other methods and/or attributes if you wish to Deck class
